ebee8216a67e1da6198f8fd16ea04f9ce11dd93f
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2024-07-12 11:26:00 +0300

    I guess this fixes the modifier issue

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ import {parse} from "./parse.ts";
 import {classname} from "./classname.ts";
 
 export {parse, classname}
-
-console.log(parse("bg-red-500/[70]"))
-console.log(classname({property: "backgroundColor", value: "red-500", modifier: "70"}))
